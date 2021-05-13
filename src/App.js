1219a372590b93a64f4a0989ede0cb77c83c7ef4
Hanh Le <hanhdtle@gmail.com>
2021-05-13 15:34:41 -0700

    added navBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1>HANH LE PORTFOLIO</h1>
+    <Navbar />
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "../logo.png"
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About Me <span className="sr-only"></span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">My Resume</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Photography</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Mixed-Media Arts</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Web Development</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact Me</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
