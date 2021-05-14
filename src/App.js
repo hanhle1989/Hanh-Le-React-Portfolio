31c616e9b4e268c341169e98c6f9147d8dcb4bdc
Hanh Le <hanhdtle@gmail.com>
2021-05-13 18:53:34 -0700

    fixed react particles

--- src/App.js
+++ src/App.js
@@ -14,14 +14,55 @@ function App() {
               value: 30,
               density: {
                 enable: true,
-                value_area: 900
+                value_area: 1000
+              }
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                encable: true,
+                speed: 0.2,
+                opacity_min: 0,
+                sync: false
+              }
+            },
+            size: {
+              value: 10,
+              random: true,
+              anim: {
+                encable: true,
+                speed: 2,
+                opacity_min: 5,
+                sync: false
+              }
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: .5,
+              direction: "top",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
               }
             }
+
           }
         }}
-          />
-    <Navbar />
-    <Header />
+      />
+      <Navbar />
+      <Header />
     </>
   );
 }
