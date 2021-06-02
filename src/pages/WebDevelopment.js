990469e176b274c9f76d1eeeeb5503fd1bf8672a
Hanh Le <hanhdtle@gmail.com>
2021-06-01 21:13:57 -0700

    still trying...

--- src/pages/WebDevelopment.js
+++ src/pages/WebDevelopment.js
@@ -2,7 +2,7 @@ import React from "react";
 import Projects from "../components/Projects";
 import projects from "../utils/projects"
 
-function WebDevelopment(){
+export const WebDevelopment = () => {
     return (
         <div>
             <Projects projects={projects}/>
